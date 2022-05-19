c57743efa918d0d226105a03ce99e8ea6bb8c9b6
Rachel Moon <hj.rachelmoon@gmail.com>
2022-05-19 12:58:16 -0700

    Part 2 in progress

--- melons.py
+++ melons.py
@@ -1,17 +1,26 @@
 """Classes for melon orders."""
 class AbstractMelonOrder:
 
-    def __init__(self, species, qty, shipped):
+    def __init__(self, species, qty, shipped, order_type,tax):
         self.species = species
         self.qty = qty
         self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
 
     def get_total(self):
         """Calculate price, including tax."""
-
+        
         base_price = 5
+
+        if self.species = "Christmas":
+            base_price *= 1.5
+
         total = (1 + self.tax) * self.qty * base_price
 
+        if self.order_type == "international" and self.qty < 10:
+            total += 3
+            
         return total
 
     def mark_shipped(self):
@@ -22,15 +31,18 @@ class AbstractMelonOrder:
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
-    tax = 0.08
-    order_type = "domestic"
+
+    def __init__(self, species, qty, shipped, "domestic", 0.08):
+        super().__init__(species, qty, shipped, order_type)
+    
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
-    tax = 9.17
-    order_type = "international"
-    country_code = country_code
+   
+    def __init__(self, species, qty, shipped, "international", country_code, 0.17):
+        super().__init__(species, qty, shipped, order_type)
+        self.country_code = country_code
 
     def get_country_code(self, country_code):
         """Return the country code."""
